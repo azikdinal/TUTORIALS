c3151e50619765e91749fb69a0194f287c11c703
azamat <dinaev2002@gmail.com>
2023-07-23 17:00:36 +0300

    update 2.43

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,9 @@
-import CustomButton from './CustomButton.jsx'
-import AIPicker from './AIPicker.jsx'
-import ColorPicker from './ColorPicker.jsx'
-import FilePicker from './FilePicker.jsx'
-import Tab from './Tab.jsx'
+import Card from './Card.jsx'
+import FormField from './FormField.jsx'
+import Loader from './Loader.jsx'
 
-export {CustomButton, AIPicker, ColorPicker, FilePicker, Tab}
+export {
+    Card,
+    FormField,
+    Loader
+}
