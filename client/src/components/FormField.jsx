64631f3fccc90b5d4b19a342e9ebc13473ded95c
azamat <dinaev2002@gmail.com>
2023-07-23 17:33:52 +0300

    update 3.06

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import {preview} from "../assets/index.js";
 
-const FormField = () => {
+const FormField = ({labelName, name, placeholder, type, value, handleChange, handleSurpriseMe, isSurpriseMe}) => {
     return (
         <div>
-            
+            <div className="flex items-center gap-2 mb-2 ">
+                <label htmlFor={name}
+                       className="block text-sm font-medium text-gray-900">
+                    {labelName}
+                </label>
+                {isSurpriseMe && (
+                    <button type="button" onClick={handleSurpriseMe}
+                            className="font-semibold text-xs bg-[#ececf1] py-1 px-2 rounded-[5px] text-black">
+                        Surpeise me
+                    </button>
+                )}
+            </div>
+            <input type={type} id={name} name={name} placeholder={placeholder} value={value} onChange={onChange}
+                   required
+                   className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] block w-full shadow-sm sm:text-sm border'/>
+            <div
+                className='relative bg-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center'>
+                {form.photo ? (
+                    <img
+                        src={form.photo}
+                        alt={form.prompt}
+                        className='w-full h-full object-contain'
+                    />
+                ) : (
+                    <img src={preview} alt="preview" className='w-9/12 h-9/12 object-contain opacity-40'/>
+                )}
+            </div>
         </div>
     );
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,23 @@
 import {useState} from "react";
 import {FormField, Loader} from "../components/index.js";
+import Card from "../components/Card.jsx";
+
+const RenderCards = ({data, title}) => {
+    if (data?.length > 0) {
+        return data.map(post => <Card key={post._id} {...post}/>)
+    }
+
+    return (
+        <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">
+            {title}
+        </h2>
+    )
+}
 
 const Home = () => {
     const [loading, setLoading] = useState(false)
     const [allPosts, setAllPosts] = useState(null);
+    const [searchText, setSearchText] = useState('');
     return (
         <section className='max-w-7xl mx-auto'>
             <div className='font-extrabold text-[@222328] text-[32px]'>
@@ -23,7 +37,26 @@ const Home = () => {
                     </div>
                 ) : (
                     <>
-
+                        {searchText && (
+                            <h2 className="font-medium text-[#666e75] text-xl mb-3">
+                                Showing results for <span className='text-[#666e75] text-xl mb-3'>
+                                    {searchText}
+                            </span>
+                            </h2>
+                        )}
+                        <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
+                            {searchText ? (
+                                <RenderCards
+                                data={[]}
+                                title="No search results found"
+                                />
+                            ): (
+                                <RenderCards
+                                    data={[]}
+                                    title="No posts found"
+                                />
+                            )}
+                        </div>
                     </>
                 )}
             </div>
