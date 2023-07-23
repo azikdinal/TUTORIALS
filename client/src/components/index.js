17acd8cc8a3f10178b46bec745fc6f6b74ec4152
azamat <dinaev2002@gmail.com>
2023-07-23 21:46:03 +0300

    update 5.02

--- client/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Card from './Card.jsx'
-import FormField from './FormField.jsx'
-import Loader from './Loader.jsx'
-
-export {
-    Card,
-    FormField,
-    Loader
-}
--- server/routes/postRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-
-dotenv.config()
-
-const router = express.Router()
-
-export default router
